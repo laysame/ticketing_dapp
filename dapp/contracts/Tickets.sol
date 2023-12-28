572dc0f70b70af5b70e08d8ddfb88e3d4c7f656f
Laysa Souza <karollinylaysa@gmail.com>
2023-12-28 21:53:10 +0000

    Added the getTicketOwner file

--- dapp/contracts/Tickets.sol
+++ dapp/contracts/Tickets.sol
@@ -15,9 +15,8 @@ contract Tickets {
     Ticket[TOTAL_TICKETS] public tickets;
     
     // Every ticket has a price and a owner
-    constructor () payable  {
-        
-        for(uint256 i =0; i < TOTAL_TICKETS; i++) {
+    constructor () payable {
+        for(uint256 i = 0; i < TOTAL_TICKETS; i++) {
             tickets[i].price = 1e17; // 0.1 ETH
             tickets[i].owner = address(0x0);
         }
@@ -27,6 +26,23 @@ contract Tickets {
         require(_index < TOTAL_TICKETS && _index >=0);
         require(tickets[_index].owner == address(0x0));
         require(msg.value >= tickets[_index].price);
+
+        // Ensure that the sent value covers both the ticket price and gas cost
+        uint256 gasCost = gasleft() * tx.gasprice;
+        require(msg.value >= tickets[_index].price + gasCost);
+
         tickets[_index].owner = msg.sender;
+
+        // Refund any excess amount
+        uint256 excessAmount = msg.value - tickets[_index].price - gasCost;
+
+        if (excessAmount > 0) {
+            payable(msg.sender).transfer(excessAmount);
+        }
+    }
+
+    function getTicketOwner(uint256 _index) external view returns (address) {
+        require(_index < TOTAL_TICKETS && _index >= 0);
+        return tickets[_index].owner;
     }
 }
